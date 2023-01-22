83f77dd40216ac6de78c715410b339353750724d
lmoberly1 <lukemoberly@gmail.com>
2023-01-22 09:14:41 -0800

    Added comments

--- vid_manager.py
+++ vid_manager.py
@@ -7,22 +7,21 @@ class VideoManager():
     def __init__(self):
         self.FRAME_OPS = FrameOperations()
 
+    # Infinite loop for video capture
     def get_vid(self, webcam_id):
-
         cap = cv.VideoCapture(webcam_id)
 
         try:
-            fps = 30
-
             # Infinite Play Loop
             while (True):
                 has_frame, frame = cap.read()
                 if cv.waitKey(1) & 0xFF == ord('q'):
                     break
                 try:
-                    # open cv code goes here
+                    # Facial detection
                     frame = self.FRAME_OPS.detect_faces(frame)
                     cv.imshow('Video Output', frame)
+
                 except Exception as e:
                     print('Exception: ', e)
                     break
