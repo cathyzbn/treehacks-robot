99b841b5b61935b8c3304cae151ecd0fde5d1d23
Cathy Zhou <cathyzbn@stanford.edu>
2023-01-22 09:52:58 -0800

    control seq + img capture

--- vid_manager.py
+++ vid_manager.py
@@ -1,30 +1,42 @@
 import cv2 as cv
 from frame_ops import FrameOperations
 
-
+def key_equal(key, key_char):
+    return key & 0xFF == ord(key_char)
 class VideoManager():
 
     def __init__(self):
         self.FRAME_OPS = FrameOperations()
 
+    def video_off(self, cap):
+        print("Turning off camera.")
+        cap.release()
+        print("Program ended.")
+        cv.destroyAllWindows()
+    
+    def video_wait(self, ticks=1600):
+        cv.waitKey(1650)
+
     # Infinite loop for video capture
-    def get_vid(self, webcam_id):
+    def get_vid(self, webcam_id, detect_face=True):
         cap = cv.VideoCapture(webcam_id)
 
-        try:
+        while (True):
+            try:
             # Infinite Play Loop
-            while (True):
                 has_frame, frame = cap.read()
-                if cv.waitKey(1) & 0xFF == ord('q'):
-                    break
-                try:
-                    # Facial detection
-                    frame = self.FRAME_OPS.detect_faces(frame)
-                    cv.imshow('Video Output', frame)
+                key = cv.waitKey(1)
+                if (detect_face):
+                    frame_with_face = self.FRAME_OPS.detect_faces(frame)
+                cv.imshow('Video Output', frame_with_face)
 
-                except Exception as e:
-                    print('Exception: ', e)
+                if key_equal(key, 'q'):
+                    self.video_off(cap)
                     break
+                elif key_equal(key, 's'):
+                    self.FRAME_OPS.img_capture(frame_with_face)
 
-        except KeyboardInterrupt:
-            print('End Program.')
+            except KeyboardInterrupt:
+                self.video_off()
+
+
